92eb1a59e1f6f7cd4ebcb42190ab0052ab5978ab
aveshkhan459 <aaveshkhan0678@gmail.com>
2026-07-18 14:57:33 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         process.env.FRONTEND_URL,
-        "http://localhost:5173",
-        "http://localhost:5174"
+        process.env.ADMIN_URL
     ],
     credentials: true
 }))

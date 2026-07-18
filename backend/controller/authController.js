5b1c23ee9ec22cc25e83d94a6b00b4f306e5d622
aveshkhan459 <aaveshkhan0678@gmail.com>
2026-07-18 14:16:36 +0530

    added

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -63,17 +63,25 @@ export const login = async (req,res) => {
     }
     
 }
-export const logOut = async (req,res) => {
-try {
-    res.clearCookie("token")
-    return res.status(200).json({message:"logOut successful"})
-} catch (error) {
-    console.log("logOut error")
-    return res.status(500).json({message:`LogOut error ${error}`})
-}
-    
-}
 
+export const logOut = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        });
+
+        return res.status(200).json({
+            message: "Logout successful"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: `Logout error ${error.message}`
+        });
+    }
+};
 
 export const googleLogin = async (req,res) => {
     try {

027cbcca3dfd95bfa0b156915672ebec076f2ef7
aveshkhan459 <aaveshkhan0678@gmail.com>
2026-07-18 22:35:37 +0530

    added

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "onecart-733d0.firebaseapp.com",
-  projectId: "onecart-733d0",
-  storageBucket: "onecart-733d0.firebasestorage.app",
-  messagingSenderId: "543692081772",
-  appId: "1:543692081772:web:555e574a01fe9bb27b2649",
+  authDomain: "foodigo-ac9df.firebaseapp.com",
+  projectId: "foodigo-ac9df",
+  storageBucket: "foodigo-ac9df.firebasestorage.app",
+  messagingSenderId: "895655093705",
+  appId: "1:895655093705:web:be4610a1653b530c057418"
 };
 
 const app = initializeApp(firebaseConfig);
